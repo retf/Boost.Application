9d0bcd60e5c67137944b38ab236dd17f58eedbab
retf <re.tf@acm.org>
2014-02-18 14:53:08 -0300

    update libs

--- include/boost/application/shared_library_load_mode.hpp
+++ include/boost/application/shared_library_load_mode.hpp
@@ -1,4 +1,4 @@
-// shared_library_mode.hpp ---------------------------------------------------//
+// shared_library_mode.hpp ----------------------------------------------------//
 // -----------------------------------------------------------------------------
 
 // Copyright 2011-2013 Renato Tegon Forti
@@ -163,11 +163,15 @@ namespace boost { namespace application {
       load_library_as_datafile                = LOAD_LIBRARY_AS_DATAFILE,            // 0x00000002
       load_library_as_datafile_exclusive      = LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE,  // 0x00000040
       load_library_as_image_resource          = LOAD_LIBRARY_AS_IMAGE_RESOURCE,      // 0x00000020
+#   ifndef _USING_V110_SDK71_
+      // when user uses: Visual Studio 2012 - Windows XP (v110_xp), we need hide following enums :
       load_library_search_application_dir     = LOAD_LIBRARY_SEARCH_APPLICATION_DIR, // 0x00000200
       load_library_search_default_dirs        = LOAD_LIBRARY_SEARCH_DEFAULT_DIRS,    // 0x00001000
       load_library_search_dll_load_dir        = LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR,    // 0x00000100
       load_library_search_system32            = LOAD_LIBRARY_SEARCH_SYSTEM32,        // 0x00000800
       load_library_search_user_dirs           = LOAD_LIBRARY_SEARCH_USER_DIRS,       // 0x00000400
+      // <-
+#   endif
       load_with_altered_search_path           = LOAD_WITH_ALTERED_SEARCH_PATH        // 0x00000008
 #elif defined( BOOST_POSIX_API )
       // posix
@@ -236,4 +240,5 @@ namespace boost { namespace application {
 }} // boost::application
 
 #endif // BOOST_APPLICATION_SHARED_LIBRARY_MODE_HPP
+
 
