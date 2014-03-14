6f4c41df18a017cd893f16d076e6dd40a6159eac
Benjamin Dieckmann <dib.github.com@all-simple.com>
2014-03-14 11:36:13 +0100

    added tests for global_context

---
+++ test/global_context_test.cpp
@@ -0,0 +1,84 @@
+// Copyright 2014 Benjamin Dieckmann
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org
+
+#define BOOST_TEST_MODULE global_context_test
+
+#include <iostream>
+#include <boost/application.hpp>
+#include <boost/test/unit_test.hpp>
+
+using namespace boost;
+
+BOOST_AUTO_TEST_CASE(create_global_context)
+{
+   application::global_context_ptr ctx =
+           application::global_context::create();
+   BOOST_CHECK(ctx.get()!=0);
+
+   application::global_context::destroy();
+}
+
+BOOST_AUTO_TEST_CASE(get_global_context)
+{
+   application::global_context_ptr ctx =
+           application::global_context::create();
+   BOOST_CHECK(application::global_context::get());
+
+   application::global_context::destroy();
+}
+
+BOOST_AUTO_TEST_CASE(destroy_global_context)
+{
+   application::global_context_ptr ctx =
+           application::global_context::create();
+
+   application::global_context::destroy();
+}
+
+BOOST_AUTO_TEST_CASE(throw_create_global_context_twice)
+{
+   application::global_context_ptr ctx = application::global_context::create();
+
+   BOOST_CHECK_THROW(application::global_context::create(), std::logic_error);
+
+   application::global_context::destroy();
+}
+
+BOOST_AUTO_TEST_CASE(throw_destroy_global_context_twice)
+{
+   application::global_context_ptr ctx =
+           application::global_context::create();
+
+   application::global_context::destroy();
+   BOOST_CHECK_THROW(application::global_context::destroy(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(throw_after_destroy_global_context)
+{
+   application::global_context_ptr ctx =
+           application::global_context::create();
+
+   application::global_context::destroy();
+   BOOST_CHECK_THROW(application::global_context::get(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(throw_before_create_global_context)
+{
+   BOOST_CHECK_THROW(application::global_context::get(), std::logic_error);
+}
+
+
+
+
+
+
+
+
+
+
+
+
